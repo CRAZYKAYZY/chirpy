b1dc619e146337bff7e7e29a5b27133260d6369c
CRAZYKAYZY <krazymusic22@gmail.com>
2023-03-26 17:36:03 +0200

    updated on handlers

--- metrics.go
+++ metrics.go
@@ -6,14 +6,15 @@ import (
 )
 
 // middleware method that implements the fileServerHits
-func (cfg *apiConfig) middlewareFileHits(next http.Handler) http.Handler {
+func (cfg *apiConfig) MiddlewareFileHits(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileServerHits++
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+// func that writes back the response with the fileServerHits
+func (cfg *apiConfig) HandlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
--- readiness.go
+++ readiness.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 // add /healthcheckz endpoint
-func handlerHealthCheck(w http.ResponseWriter, r *http.Request) {
+func HandlerHealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK NIGGA"))
