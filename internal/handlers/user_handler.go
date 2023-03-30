d6c0f6bdbd2a6b88235e5fada5543cf6f1e2dcef
CRAZYKAYZY <krazymusic22@gmail.com>
2023-03-30 23:08:02 +0200

    added users handlers

---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,92 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/CRAZYKAYZY/chirpy/internal/database"
+	"github.com/go-chi/chi/v5"
+)
+
+type UserRes struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+}
+
+func CreateUserHandler(db *database.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req database.User
+
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		chirp, err := db.CreateUser(req.Email, req.Password)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Write the response
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		res := UserRes{
+			ID:    chirp.ID,
+			Email: chirp.Email,
+		}
+
+		json.NewEncoder(w).Encode(res)
+	}
+}
+
+func GetUsersHandler(db *database.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			return
+		}
+
+		user, err := db.GetUser(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		res := UserRes{
+			ID:    user.ID,
+			Email: user.Email,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func GetAllUsersHandler(db *database.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := db.GetUsers()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var res []UserRes
+		for _, user := range users {
+			res = append(res, UserRes{
+				ID: user.ID,
+			})
+		}
+
+		respondWithJSON(w, http.StatusOK, res)
+	}
+}
